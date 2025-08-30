c0de28d4d0b20fa7af459975f5a9418b89a22ed2
JZou-Code <jzou2871@gmail.com>
2025-08-30 23:52:14 +1200

    Update.

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,40 +1,12 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext} from 'react';
 import classes from '../style/AccountPage.module.css'
-import axios from "axios";
-// import axiosApi from "../api/axiosApi.js";
 import {useNavigate} from "react-router-dom";
 import AuthContext from "../context/AuthContext.jsx";
 import {roles} from "../utils/roles.js";
-import useAuth from "../hook/useAuth.jsx";
 
 const AccountPage = () => {
-    const [msg, setMsg] = useState(null)
     const navigate = useNavigate();
     const authCtx = useContext(AuthContext);
-
-    const {refreshToken} = useAuth();
-
-    const test = async () => {
-        try {
-            const token = JSON.parse(localStorage.getItem('auth') || '{}')?.token || '';
-
-            console.log(token)
-
-            const res = await axios.post('http://localhost:3000/user/profile',
-                {},
-                {
-                    headers: token ? {Authorization: `Bearer ${token}`} : {}
-                });
-            console.log(res)
-            setMsg(`OK ${res.status} — ${JSON.stringify(res.data)}`);
-        } catch (e) {
-            console.log(e)
-            if (e.status === 401){
-                refresh()
-            }
-            // setMsg(`ERR ${e?.response?.status} — ${e?.response?.data?.message || e.message}`);
-        }
-    }
 
     const navigateToNewsletter = () => {
         navigate('/newsletter')
@@ -46,14 +18,6 @@ const AccountPage = () => {
 
     const navigateToAdmin = () => {
         navigate('/admin-dashboard')
-    }
-
-    const navigateToBind = () => {
-        navigate('/account/bind-child')
-    }
-
-    const refresh = async ()=>{
-        await refreshToken();
     }
 
     return (
@@ -93,7 +57,7 @@ const AccountPage = () => {
                         <button
                             onClick={navigateToChildMessage}
                             className={classes.Button}>
-                            hello - {msg}
+                            Go
                         </button>
                     </div>
                 </div>
@@ -137,31 +101,6 @@ const AccountPage = () => {
                                 onClick={navigateToAdmin}
                                 className={classes.Button}>
                                 Manage
-                            </button>
-                        </div>
-                    </div>
-                    <div className={classes.ImageContainer}>
-                        <img className={classes.Image} alt='Newsletter'
-                             src='../public/images/account-page/newsletter.jpg'/>
-                    </div>
-                </section>
-            }
-            {
-                authCtx.user.role === roles.PARENT &&
-                <section className={classes.ContentBlock}>
-                    <div className={classes.Content}>
-                        <div className={classes.Title}>
-                            Admin Dashboard
-                        </div>
-                        <div className={classes.Text}>
-                            This section is restricted to staff only. Here, administrators can monitor system activity,
-                            manage accounts, and update important kindergarten information."
-                        </div>
-                        <div className={classes.ButtonContainer}>
-                            <button
-                                onClick={navigateToBind}
-                                className={classes.Button}>
-                                Bind Child
                             </button>
                         </div>
                     </div>
--- client/src/pages/ChildMessage.jsx
+++ client/src/pages/ChildMessage.jsx
@@ -1,11 +1,121 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import classes from '../style/ChildMessage.module.css'
 import ChildrenList from "../components/ChildrenList.jsx";
+import {forms} from "../utils/forms.js";
+import UsersList from "../components/UsersList.jsx";
+import BindChildToParent from "../components/BindChildToParent.jsx";
+import {fetchChildren, fetchUsers} from "../api/adminOperation.js";
+import {deleteUserById} from "../api/manageUsers.js";
+import {deleteChildById} from "../api/manageChildren.js";
 
 const ChildMessage = () => {
+    const [pageIndex, setPageIndex] = useState(0);
+    const [itemNum, setItemNum] = useState(10);
+    const [orderBy, setOrderBy] = useState({createdAt: 'desc'});
+    const [isLoading, setIsLoading] = useState(false);
+    const [children, setChildren] = useState([]);
+
+    const [disablePrev, setDisablePrev] = useState(true);
+    const [disableNext, setDisableNext] = useState(false);
+
+    const loadChildren = useCallback(async () => {
+        try {
+            setIsLoading(true);
+            const res = await fetchChildren(pageIndex, itemNum, orderBy);
+            const arr = res.data?.data ?? [];
+            if (arr) {
+                arr.map(item => {
+                    item.dob = item.dob.split('T')[0];
+                })
+            }
+            setChildren(arr.slice(0, itemNum));
+
+            setDisablePrev(pageIndex === 0);
+            setDisableNext(arr.length <= itemNum);
+        } catch (e) {
+            console.log(e);
+        } finally {
+            setIsLoading(false);
+        }
+    }, [pageIndex, itemNum, orderBy]);
+
+    useEffect(() => {
+        loadChildren();
+    }, [loadChildren]);
+
+    const handlePrev = () => {
+        setDisableNext(false);
+        if (pageIndex < itemNum) {
+            return
+        }
+        setPageIndex(pageIndex - itemNum);
+
+        if (pageIndex <= itemNum) {
+            setDisablePrev(true);
+        }
+    }
+
+    const handleNext = () => {
+        setDisablePrev(false);
+        setPageIndex(pageIndex + itemNum)
+    }
+
     return (
         <div className={classes.Container}>
-            <ChildrenList/>
+            <div className={classes.ListContainer}>
+                {
+                    isLoading ?
+                        <div>Loading...</div>
+                        :
+                        <table>
+                            <thead>
+                            <tr>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Date of Birth</th>
+                                <th>Gender</th>
+                                <th>Parent</th>
+                                <th>Message</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {
+                                children && children.map((item, index) =>
+                                    <tr key={item.id}>
+                                        <td>{item.firstName}</td>
+                                        <td>{item.lastName}</td>
+                                        <td>{item.dob}</td>
+                                        <td>{item.gender}</td>
+                                        <td>{item.parent}</td>
+                                        <td className={`${classes.CellButtonContainer}`}>
+                                            <button datasrc={index}
+                                                    onClick={() => {
+                                                        props.setChild(item)
+                                                    }}
+                                                    className={classes.Button}>
+                                                Message
+                                            </button>
+                                        </td>
+                                    </tr>
+                                )
+                            }
+                            </tbody>
+                        </table>
+                }
+                <div className={classes.ListFunction}>
+                    <button
+                        onClick={handlePrev}
+                        className={disablePrev ? `${classes.Button} ${classes.Disabled}` : classes.Button}>
+                        Previous
+                    </button>
+                    <button
+                        onClick={handleNext}
+                        className={disableNext ? `${classes.Button} ${classes.Disabled}` : classes.Button}>
+                        Next
+                    </button>
+                </div>
+            </div>
+
         </div>
     );
 };
