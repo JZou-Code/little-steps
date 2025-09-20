98917612f604235204378f8f6d2db0b79b53ac01
JZou <jzou2871@gmail.com>
2025-09-20 16:12:14 +1200

    Use custom axios instance to send request.

---
+++ server/src/routes/topRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const userRouter = require('./userRoutes');
+const childRouter = require('./childRoutes');
+const messageRouter = require('./messageRoutes');
+const newsletterRouter = require('./newsletterRoutes');
+const commentRouter = require('./commentRoutes');
+
+router.use('/user', userRouter);
+router.use('/child', childRouter);
+router.use('/message', messageRouter);
+router.use('/newsletter', newsletterRouter);
+router.use('/comment', commentRouter);
+
+module.exports = router;
